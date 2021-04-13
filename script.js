71b2d92f4f8211e12331a64cd55def401f2be990
measifalam <measifalam@gmail.com>
2021-04-13 07:46:55 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var name=prompt('Enter your Name:');
+var age=prompt('Enter your age:');
+
+agedays=age*365.25;
+remainingdays=(60-age)*365.25;
+alert("Hello "+name+"...\nYour Current Age:"+agedays+" days\nYou will be there on the earth only "+remainingdays+" days. No one can change including God also");
